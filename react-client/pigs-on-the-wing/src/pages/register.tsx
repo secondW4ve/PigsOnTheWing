da5544165e52bc357e512d4e2b3d4ec0549612ee
Yurii Shchur <yshchur@mycubes.nl>
2022-09-15 09:47:21 +0300

    Add register page

--- react-client/pigs-on-the-wing/src/pages/register.tsx
+++ react-client/pigs-on-the-wing/src/pages/register.tsx
@@ -1,12 +1,49 @@
 import React from 'react';
+import { Form, Formik } from 'formik';
+import { Box, Button, FormControl } from '@chakra-ui/react';
+import Wrapper from '../components/Wrapper';
+import { InputField } from '../components/InputField';
 
 interface registerProps {
 
 }
 
-export const Register: React.FC<registerProps> = ({}) => {
+export const Register: React.FC<registerProps> = ({ }) => {
   return (
-    <div>Register page</div>
+    <Wrapper>
+      <Formik
+        initialValues={{username: '', password: ''}}
+        onSubmit={(values) => {
+          console.log(values)
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <FormControl>
+              <InputField
+                name='username'
+                placeholder='username'
+                label='Username'
+              />
+              <Box mt={4}>
+                <InputField
+                  name='password'
+                  placeholder='password'
+                  label='Password'
+                  type='password'
+                />
+              </Box>
+              <Button
+                mt={4}
+                type='submit'
+                colorScheme={'teal'}
+                isLoading={isSubmitting}
+              >Register</Button>
+            </FormControl>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
   );
 }
 
